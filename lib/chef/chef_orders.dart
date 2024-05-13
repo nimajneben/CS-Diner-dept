5463ec6931e09f0d83594708d4e7e15e142f46fb
junfenglau <153343909+junfenglau@users.noreply.github.com>
2024-05-13 18:10:55 -0400

    modified from items array to items map

--- lib/chef/chef_orders.dart
+++ lib/chef/chef_orders.dart
@@ -1,71 +1,65 @@
 import 'package:flutter/material.dart';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
-// logout button, redirect to login.dart
 import 'package:manju_restaurant/pages/login.dart';
+import 'package:manju_three/chef/chef_home.dart';
 
 
-// SERVE ORDERS -- (currently) IRRESPECTIVE of WHICH CHEF is named
-//       >> Display All Orders (Menu Item Names)
-//       >> 'SERVED' Button
+// SERVE ORDERS -- 
+//       >> Display All Orders (Menu Item Names) -- shows items they're responsible for
+//       >> 'SERVED' Button -- click after serving all items
 //       >> Firestore 'Orders' database : 
 //                >> 'isOrderComplete' == true
-
-// NOTE 0 : in 'Menu', currently queries for 'Name' field or 'itemName' field
-//          in 'Orders', assumes 'order' array exists; collection of 'Menu' item references
-
-// NOTE 1 : Can't Update the Ingredients Inventory AUTOMATICALLY
-//              if 'Menu' items aren't created with an 'Ingredients' array,
-//          SO---- Chefs MUST Update the Ingredients Inventory MANUALLY (<< currently method )
-//          OR---- Could make a 'Menu-Ingre' database; each document is a Menu item;
-//                  has an array of Ingredients; & use Ingredients array to update Inventory
-//              BUT !! INGREDIENTS database needs to be populated upon 'Menu' item creation 
-//
+//                >> 'items' map -- name (string), price (number), quantity (number)
 
 /* IDEA : 
 
-<<   Pending Orders   [LOGOUT]
-____________________________________
-
-Order ID: (ABC123)
-Menu Item: (Item 1)
-Menu Item: (Item 2)
-Menu Item: (Item 3)
-[SERVED]
-
-Order ID: (DEF000)
-Menu Item: (Item 4)
-Menu Item: (Item 5)
-[SERVED]
-
-Order ID: (XYZ999)
-Menu Item: (Item 6)
-Menu Item: (Item 7)
-Menu Item: (Item 8)
-Menu Item: (Item 9)
-[SERVED]
-
-... [ cont'd list of orders    ]
-... [ from 'Orders' collection ]
+|  <<     Pending Orders     [LOGOUT]   |
+_________________________________________
+|                                       |
+|  Order ID: 001                        |
+|  Item: Veggie Burger, Quantity: 2     |
+|  Item: Caesar Salad, Quantity: 1      |
+|                                       |
+|              [SERVED]                 |
+|                                       |
+-----------------------------------------
+|                                       |
+|  Order ID: 002                        |
+|  Item: Beef Ramen, Quantity: 3        |
+|  Item: Fried Rice, Quantity: 2        |
+|                                       |
+|              [SERVED]                 |
+|                                       |
+-----------------------------------------
+|                                       |
+|  Order ID: 003                        |
+|  Item: Coke, Quantity: 1              |
+|  Item: Garlic Bread, Quantity: 4      |
+|                                       |
+|              [SERVED]                 |
+|                                       |
+-----------------------------------------
+|               ...                     |
+_________________________________________
 
 */
 
 
+class Item {
+  final String name;
+  final int quantity;
+
+  Item({required this.name, required this.quantity});
+}
 
 class Order {
-  final String id;
-  final bool isOrderComplete;
-  final List<DocumentReference> order;
+    final String orderId;
+    final bool isOrderComplete;
+    final List<Item> items;
 
-  Order({required this.id, required this.isOrderComplete, required this.order});
-}
-
-class MenuItem {
-  final String name;
-  MenuItem({required this.name});
-}
+    Order({required this.orderId, required this.isOrderComplete, required this.items});
+  }
 
 class OrdersPage extends StatelessWidget {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -77,6 +71,12 @@ class OrdersPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => ChefHome())
+          ),
+        ),
         backgroundColor: Colors.redAccent,
         title: Text("Pending Orders", style: TextStyle(fontWeight: FontWeight.bold)),
         centerTitle: true,
@@ -98,31 +98,25 @@ class OrdersPage extends StatelessWidget {
             return Center(child: CircularProgressIndicator());
           }
 
-          final orderRef = snapshot.data!.docs.map((doc) {
-            final order = List<DocumentReference>.from(doc['order']);
-            return Order(id: doc.id, isOrderComplete: doc['isOrderComplete'], order: order);
+          final orders = snapshot.data!.docs.map((doc) {
+            final itemsMap = Map<String, dynamic>.from(doc['items']);
+            final List<Item> items = itemsMap.entries.map((entry) {
+              return Item(name: entry.value['itemName'], quantity: entry.value['quantity']);
+            }).toList();
+            return Order(orderId: doc.id, isOrderComplete: doc['isOrderComplete'], items: items);
           }).toList();
 
           return ListView.builder(
-            itemCount: orderRef.length,
+            itemCount: orders.length,
             itemBuilder: (context, index) {
-              final order = orderRef[index];
+              final order = orders[index];
               return ListTile(
                 title: Text('Order ID: ${order.id}'),
-                subtitle: FutureBuilder<List<MenuItem>>(
-                  future: _getMenuItems(order.order, chefID!),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return CircularProgressIndicator();
-                    }
-                    final menuItems = snapshot.data ?? [];
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        for (final menuItem in menuItems) Text('Menu Item: ${menuItem.name}'), // .name ?
-                      ],
-                    );
-                  },
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    for (final item in order.items) Text('Item: ${item.name}, Quantity: ${item.quantity}'),
+                  ],
                 ),
                 trailing: ElevatedButton(
                   onPressed: () {
@@ -140,22 +134,6 @@ class OrdersPage extends StatelessWidget {
         },
       ),
     );
-  }
-
-  Future<List<MenuItem>> _getMenuItems(List<DocumentReference> orderReferences, String chefID) async {
-    final menuItems = <MenuItem>[];
-    for (final reference in orderReferences) {
-      try {
-        final snapshot = await reference.get();
-        if (snapshot.exists && snapshot['chefID'] == chefID) {
-          final menuItemName = snapshot['Name'] ?? snapshot['itemName'];    // checks for 'Name' or 'itemName'
-          menuItems.add(MenuItem(name: menuItemName));
-        }
-      } catch (e) {
-        print('Error fetching menu item: $e');
-      }
-    }
-    return menuItems;
   }
 
   void _logout(BuildContext context) async {
