45fd00104d902e95dc869fce33dab4f9413e80be
Benjamin <47288387+nimajneben@users.noreply.github.com>
2024-05-13 12:28:51 -0400

    add profile page
    re-used code from fardeen's profile page
    
    Co-Authored-By: Fardeen Ahmed <123484922+Fardeen31@users.noreply.github.com>

--- lib/screens/importer_menu.dart
+++ lib/screens/importer_menu.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:manju_three/pages/surfer_home.dart';
 import 'package:manju_three/screens/importer_import_screen.dart';
 import 'package:manju_three/screens/complaints_screen.dart';
 import 'package:manju_three/screens/importer_ingredients_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:manju_three/methods/data.dart';
+
+// TODO: Retreive Username
+// String _getUserName() {
+//   String un = 'Username';
+//   var snapshot =
+//       DatabaseFunctions().getChefName(FirebaseAuth.instance.currentUser!.uid);
+//   snapshot.then(
+//     (value) {
+//       un = value.;
+//     },
+//   );
+//   return un;
+// }
 
 class ImporterMainScreen extends StatefulWidget {
-  const ImporterMainScreen({super.key, required this.title});
-  final String title;
+  const ImporterMainScreen({super.key});
 
   @override
   State<ImporterMainScreen> createState() => _ImporterMainScreenState();
@@ -13,7 +29,6 @@ class ImporterMainScreen extends StatefulWidget {
 
 class _ImporterMainScreenState extends State<ImporterMainScreen> {
   int currentPageIndex = 0;
-  String profileName = 'User';
 
   static const double cardHeight = 100.0;
   static const double cardWidth = 300.0;
@@ -43,59 +58,114 @@ class _ImporterMainScreenState extends State<ImporterMainScreen> {
           selectedIndex: currentPageIndex,
         ),
         appBar: AppBar(
-          title: Text('Hi, Importer $profileName'),
+          title: Text('Hi, Importer Username'),
         ),
-        body: GridView.count(
-            primary: false,
-            padding: const EdgeInsets.all(20),
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            crossAxisCount: 2,
-            children: <Widget>[
-              InkWell(
-                  child: const Card(
+        body: <Widget>[
+          GridView.count(
+              primary: false,
+              padding: const EdgeInsets.all(20),
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              crossAxisCount: 2,
+              children: <Widget>[
+                InkWell(
+                    child: const Card(
+                        child: SizedBox(
+                            width: cardWidth,
+                            height: cardHeight,
+                            child: Center(child: Text('Food Purchases')))),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  const ImporterImportScreen()));
+                    }),
+                InkWell(
+                    child: const Card(
+                        child: SizedBox(
+                            width: cardWidth,
+                            height: cardHeight,
+                            child: Center(child: Text('Query Inventory')))),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  const ImporterIngredientsView()));
+                    }),
+                InkWell(
+                    child: const Card(
                       child: SizedBox(
                           width: cardWidth,
                           height: cardHeight,
-                          child: Center(child: Text('Food Purchases')))),
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder:
-                            (context) => const ImporterImportScreen()));
-                  }),
-              InkWell(
-                  child: const Card(
-                      child: SizedBox(
-                          width: cardWidth,
-                          height: cardHeight,
-                          child: Center(child: Text('Query Inventory')))),
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder:
-                            (context) => const ImporterIngredientsView()));
-                  }),
-              InkWell(
-                child: const Card(
-                  child: SizedBox(
-                      width: cardWidth,
-                      height: cardHeight,
-                      child: Center(child: Text('View Complaints'))),
+                          child: Center(child: Text('View Complaints'))),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const ComplaintScreen()));
+                    }),
+                const Card(
+                    child: SizedBox(
+                        width: cardWidth,
+                        height: cardHeight,
+                        child: Center(child: Text('Complaint Form')))),
+                const Card(
+                    child: SizedBox(
+                        width: cardWidth,
+                        height: cardHeight,
+                        child: Center(child: Text('Dispute Complaints')))),
+              ]),
+          Center(
+              child: Column(
+            children: [
+              Center(
+                child: Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 20),
+                      child: CircleAvatar(
+                        radius: 50,
+                        backgroundImage: AssetImage("images/profile1.jpg"),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Name: ${'John Doe'}",
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "Email: ${'john@johm.com'}",
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
-                onTap: () {
-                  Navigator.push(context,
-                    MaterialPageRoute(builder:
-                        (context) => const ComplaintScreen()));
-                }),
-              const Card(
-                  child: SizedBox(
-                      width: cardWidth,
-                      height: cardHeight,
-                      child: Center(child: Text('Complaint Form')))),
-              const Card(
-                  child: SizedBox(
-                      width: cardWidth,
-                      height: cardHeight,
-                      child: Center(child: Text('Dispute Complaints')))),
-            ]));
+              ),
+              ElevatedButton(
+                child: const Text('Sign Out'),
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SurferHome()),
+                      (route) => false);
+                },
+              )
+            ],
+          ))
+        ][currentPageIndex]);
   }
 }
