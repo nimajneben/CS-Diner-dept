88f2a65a7ee31301fbe94faeec450e4c6ec8c7bd
Benjamin <47288387+nimajneben@users.noreply.github.com>
2024-05-13 19:29:09 -0400

    apply vscode refactoring

--- lib/methods/data.dart
+++ lib/methods/data.dart
@@ -1,28 +1,28 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class DatabaseFunctions{
-
-
+class DatabaseFunctions {
   /**
    * Function used to add menu item to the database,
    * need to pass a map of the item data and the function will add it to the database
    * the pictures are stored in the firebase storage and the url is stored in the database
    */
   Future addMemuItem(Map<String, dynamic> itemData) async {
-    return await FirebaseFirestore.instance.collection("Menu").doc()
-      .set(itemData);
-    }
+    return await FirebaseFirestore.instance
+        .collection("Menu")
+        .doc()
+        .set(itemData);
+  }
 
 /**
  * 
  * this function is a stre
  */
-  Future <Stream<QuerySnapshot>> getMenuItems(String category) async{
+  Future<Stream<QuerySnapshot>> getMenuItems(String category) async {
     return await FirebaseFirestore.instance
-    .collection("Menu")
-    .where("category", isEqualTo: category)
-    .snapshots();
+        .collection("Menu")
+        .where("category", isEqualTo: category)
+        .snapshots();
   }
   /**
  * 
@@ -31,14 +31,14 @@ class DatabaseFunctions{
  * 
  */
 
-Future <Stream<QuerySnapshot>> getTopRatedItems(String category) async{
-  return await FirebaseFirestore.instance
-    .collection("Menu")
-    .where('category', isEqualTo: category)
-    .orderBy("rating", descending: true)
-    .limit(3)
-    .snapshots();
-}
+  Future<Stream<QuerySnapshot>> getTopRatedItems(String category) async {
+    return await FirebaseFirestore.instance
+        .collection("Menu")
+        .where('category', isEqualTo: category)
+        .orderBy("rating", descending: true)
+        .limit(3)
+        .snapshots();
+  }
 
 /**
  * 
@@ -47,11 +47,12 @@ Future <Stream<QuerySnapshot>> getTopRatedItems(String category) async{
  * 
  */
 
-Future<String> getUserRole(String uid) async {
-  return await FirebaseFirestore.instance.collection("users")
-    .doc(uid)
-    .get()
-    .then((value) {
+  Future<String> getUserRole(String uid) async {
+    return await FirebaseFirestore.instance
+        .collection("users")
+        .doc(uid)
+        .get()
+        .then((value) {
       var data = value.data();
       if (data != null) {
         return data['role'];
@@ -60,20 +61,20 @@ Future<String> getUserRole(String uid) async {
         throw Exception('User data is null');
       }
     });
-}
+  }
 
 //**
 // Universal logout button
 //
 // */
 
-Future<void> logout() async{
-  try{
-    await FirebaseAuth.instance.signOut();
-  } catch(e){
-    print(e.toString());
+  Future<void> logout() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+    } catch (e) {
+      print(e.toString());
+    }
   }
-}
 
 /**
  * 
@@ -81,11 +82,12 @@ Future<void> logout() async{
  * 
  */
 
-Future<String> getChefName(String uid) async {
-  return await FirebaseFirestore.instance.collection("users")
-    .doc(uid)
-    .get()
-    .then((value) {
+  Future<String> getChefName(String uid) async {
+    return await FirebaseFirestore.instance
+        .collection("users")
+        .doc(uid)
+        .get()
+        .then((value) {
       var data = value.data();
       if (data != null) {
         return data['name'];
@@ -94,12 +96,7 @@ Future<String> getChefName(String uid) async {
         throw Exception('User data is null');
       }
     });
-
-
   }
-
-
-
 
 /**
  * 
@@ -109,10 +106,11 @@ Future<String> getChefName(String uid) async {
 */
 
   Future<dynamic> getApprovalInfo(String uid) async {
-  return await FirebaseFirestore.instance.collection("users")
-    .doc(uid)
-    .get()
-    .then((value) {
+    return await FirebaseFirestore.instance
+        .collection("users")
+        .doc(uid)
+        .get()
+        .then((value) {
       var data = value.data();
       if (data != null) {
         return data['isApproved'];
@@ -121,11 +119,7 @@ Future<String> getChefName(String uid) async {
         throw Exception('User data is null');
       }
     });
-}
-
-
-
-
+  }
 
 /**
  * 
@@ -133,12 +127,12 @@ Future<String> getChefName(String uid) async {
  * 
  */
 
-
- Future<dynamic> getUserWallet(String uid) async {
-  return await FirebaseFirestore.instance.collection("users")
-    .doc(uid)
-    .get()
-    .then((value) {
+  Future<int> getUserWallet(String uid) async {
+    return await FirebaseFirestore.instance
+        .collection("users")
+        .doc(uid)
+        .get()
+        .then((value) {
       var data = value.data();
       if (data != null) {
         return data['deposit'];
@@ -147,32 +141,29 @@ Future<String> getChefName(String uid) async {
         throw Exception('User data is null');
       }
     });
-}
+  }
 
+  Future<void> addMoneyToUserWallet(String uid, double amountToAdd) async {
+    var userRef = FirebaseFirestore.instance.collection("users").doc(uid);
+    var transactionResponse =
+        await FirebaseFirestore.instance.runTransaction((transaction) async {
+      var userSnapshot = await transaction.get(userRef);
+      if (!userSnapshot.exists) {
+        throw Exception('User does not exist');
+      }
 
+      var userData = userSnapshot.data();
+      if (userData != null && userData['deposit'] != null) {
+        double currentWallet = userData['deposit'] as double;
+        double updatedWallet = currentWallet + amountToAdd;
+        transaction.update(userRef, {'wallet': updatedWallet});
+      } else {
+        throw Exception('Wallet data is missing or null');
+      }
+    }).catchError((error) {
+      throw Exception('Failed to update wallet: $error');
+    });
 
-Future<void> addMoneyToUserWallet(String uid, double amountToAdd) async {
-  var userRef = FirebaseFirestore.instance.collection("users").doc(uid);
-  var transactionResponse = await FirebaseFirestore.instance.runTransaction((transaction) async {
-    var userSnapshot = await transaction.get(userRef);
-    if (!userSnapshot.exists) {
-      throw Exception('User does not exist');
-    }
-
-
-    var userData = userSnapshot.data();
-    if (userData != null && userData['deposit'] != null) {
-      double currentWallet = userData['deposit'] as double;
-      double updatedWallet = currentWallet + amountToAdd;
-      transaction.update(userRef, {'wallet': updatedWallet});
-    } else {
-      throw Exception('Wallet data is missing or null');
-    }
-  }).catchError((error) {
-    throw Exception('Failed to update wallet: $error');
-  });
-
-  return transactionResponse;
-}
-
+    return transactionResponse;
+  }
 }
