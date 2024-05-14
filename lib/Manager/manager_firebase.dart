94c0915c0e5bdc1a49f4d054e066e8c195096fab
Shamia <shamiashanaha8@gmail.com>
2024-05-14 12:35:04 -0400

    here is the new edits I made

---
+++ lib/Manager/manager_firebase.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+
+class FirebaseManager {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<void> addComplaint({
+    required String name,
+    required DateTime date,
+    required String location,
+    required String address,
+    required String status,
+    required String description,
+    required String accuseName,
+  }) async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user == null) return;
+
+    String collectionName = 'complaints'; // Set the collection name to 'complaints'
+
+    Map<String, dynamic> data = {
+      'filersId': user.uid,
+      'timestamp': Timestamp.now(),
+      'name': name,
+      'date': Timestamp.fromDate(date), // Convert DateTime to Timestamp
+      'location': location,
+      'address': address,
+      'status': status,
+      'description': description,
+      'accuseName': accuseName,
+      'appealed_verdict': "unknown",
+      'category': "what type of complaint",
+      'mgr_reviewed': false,
+      'mgr_verdict': "innocent",
+      'subject': "what the user is complaining about-- could be an order id, menu id, procurement id, etc."
+    };
+
+    await FirebaseFirestore.instance.collection(collectionName).add(data).then((documentSnapshot) =>
+        print("Added Data with ID: ${documentSnapshot.id}")); // Add .then callback here
+  }
+}
+
+
--- lib/Manager/receipt.dart
+++ lib/Manager/receipt.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 
-class receipt extends StatelessWidget {
+class Receipt extends StatelessWidget {
   final String name;
   final DateTime date;
   final String location;
   final String address;
   final String address2;
   final String description;
+  final String accuseName; // New field
 
-  receipt({
+  Receipt({
     required this.name,
     required this.date,
     required this.location,
     required this.address,
     required this.address2,
     required this.description,
+    required this.accuseName, // New field
   });
 
   @override
@@ -29,7 +31,7 @@ class receipt extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Text('Name: $name', style: TextStyle(fontSize: 20)),
-            Text('Date: ${date.day}/${date.month}/${date.year}',
+            Text('Date: ${date.month}/${date.day}/${date.year}',
                 style: TextStyle(fontSize: 20)),
             Text('Status: $address2',
                 style: TextStyle(fontSize: 20)), // New field
@@ -37,6 +39,7 @@ class receipt extends StatelessWidget {
             Text('Address Complain To: $address',
                 style: TextStyle(fontSize: 20)),
             Text('Description: $description', style: TextStyle(fontSize: 20)),
+            Text('Accused: $accuseName', style: TextStyle(fontSize: 20)), // Display the new field
           ],
         ),
       ),
